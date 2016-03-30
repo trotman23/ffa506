8b3d86b47099023a74ddfc20886d8669a3af540f
Jake Trotman <trotman23@gmail.com>
2016-03-29 20:48:43 -0500

    forgot to check this in i guess
    a

--- src/main/java/com/ffa/controllers/APIController.java
+++ src/main/java/com/ffa/controllers/APIController.java
@@ -5,12 +5,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.ffa.models.*;
+import java.util.*;
 
 @RestController
 public class APIController {
 
-	@RequestMapping("/NFLTeam")
+	private static final String rest = "/rest/";
+	
+	@RequestMapping(rest + "NFLTeam")
     public NFLTeam team(@RequestParam(value="TeamID", defaultValue = "1") String id) {
         return new NFLTeam(Integer.parseInt(id));
     }
+	
+	@RequestMapping(rest + "Roster")
+	public List<Roster> roster(
+			@RequestParam(value="LeagueID", defaultValue = "1") String LeagueID, 
+			@RequestParam(value = "TeamID", defaultValue = "1") String TeamID,
+			@RequestParam(value = "Week", defaultValue = "1") String Week,
+			@RequestParam(value = "Year", defaultValue = "1") String Year
+			) {
+		Roster r = new Roster();
+		return r.getRoster(LeagueID, TeamID, Week, Year);
+	}
 }
