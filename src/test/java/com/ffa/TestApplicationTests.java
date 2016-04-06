27d1ba4bb659a2b2250fe6b77175deb90f98dee9
Joseph Bauer <jwbauer3@wisc.edu>
2016-04-06 15:49:13 -0500

    Refactored Tests to make more sense

--- src/test/java/com/ffa/TestApplicationTests.java
+++ src/test/java/com/ffa/TestApplicationTests.java
@@ -23,18 +23,20 @@ public class TestApplicationTests {
 	public void contextLoads() {
 	}
 
+	
+	//Test Fair Trade Judge Tool
 	@Test
 	public void testFTJ(){
 		int adrianPeterson = 1966936255;
 		int eddieLacy= 1221912022;
-		
+
 		FtjStats ftj = new FtjStats();
-		
+
 		//Test two players with point differential that will return unfair
 		boolean unfair = ftj.isFair(adrianPeterson, eddieLacy);
 		assertFalse(unfair);
 		System.out.println("Unfair trade logic pass");
-		
+
 		//Test player being traded with himself should return fair as point 
 		//total will be identical
 		boolean fair = ftj.isFair(adrianPeterson, adrianPeterson);
@@ -45,16 +47,17 @@ public class TestApplicationTests {
 		assertFalse(unfair);
 		System.out.println("invaild player returns unfair");
 
-		
-		
-				
+
+
+
 	}
-	
+
+	//Sanity Check of Roster Values
 	@Test
-	public void testRosterModel(){
+	public void testRosterSanityCheck(){
 		//Testing Object
 		Roster testRoster = new Roster();
-		
+
 		//Testing Constants
 		final int ROSTER_SIZE=12;
 		final int LEAGUE_SIZE=12;
@@ -69,16 +72,16 @@ public class TestApplicationTests {
 		int team_id;
 		List<Roster> tr;
 
-		
+
 		//Roster Class Sanity Check
 		for(team_id=INTIAL_TEAM; team_id<LEAGUE_SIZE; team_id++){
 			for(week=INTIAL_WEEK; week<=SEASON_LENGTH; week++){
-				 tr=testRoster.getRoster(League_ID, 
+				tr=testRoster.getRoster(League_ID, 
 						Integer.toString(team_id),Integer.toString(week), YEAR);
 				for(Roster currPlayer: tr){
 					assertNotNull(currPlayer);
-					assertEquals(tr.size(), ROSTER_SIZE);
-					
+					assertTrue(tr.size()== ROSTER_SIZE ||tr.size()== ROSTER_SIZE+1);
+
 					//Check valid values
 					assertEquals(currPlayer.Teams_FFATeamID,team_id);
 					assertEquals(currPlayer.Teams_Leagues_LeagueID, Integer.parseInt(League_ID));
@@ -99,16 +102,51 @@ public class TestApplicationTests {
 					assertTrue(currPlayer.NFLTeam_NFLTeamID>0);
 				}
 			}
-			
+
 		}
 		System.out.println("Sanity Check on Roster Class Passes");
-		
-		
-		
-		//Get intial roster from team one for next two tests
-		 tr=testRoster.getRoster(League_ID, 
-				 Integer.toString(INTIAL_TEAM),Integer.toString(INTIAL_WEEK), YEAR);
-		
+	}
+
+	//Check for a complete roster
+	@Test
+	public void testRosterCompleteRosterTest(){
+		Roster testRoster = new Roster();
+		List<Roster> tr;
+		final String INTIAL_TEAM="1";
+		final String INTIAL_WEEK="1";
+		final String YEAR="2015";
+		final String League_ID="1682132";
+
+		tr=testRoster.getRoster(League_ID,INTIAL_TEAM,INTIAL_WEEK,YEAR);
+
+		//test complete roster
+		assertEquals(tr.get(0).Name, "Ryan Tannehill");
+		assertEquals(tr.get(1).Name, "Eric Decker");
+		assertEquals(tr.get(2).Name, "Jamaal Charles");
+		assertEquals(tr.get(3).Name, "Giovani Bernard");
+		assertEquals(tr.get(4).Name, "Dorial Green-Beckham");
+		assertEquals(tr.get(5).Name, "Martavis Bryant");
+		assertEquals(tr.get(6).Name, "Eric Ebron");
+		assertEquals(tr.get(7).Name, "Randall Cobb");
+		assertEquals(tr.get(8).Name,"Dan Bailey");
+		assertEquals(tr.get(9).Name, "Roddy White");
+		assertEquals(tr.get(10).Name, "Tom Brady");
+		assertEquals(tr.get(11).Name, "Alshon Jeffery");
+		System.out.println("Complete roster test passes");	
+	}
+
+	//Check for complete player
+	@Test
+	public void testCompletePlayer(){
+		Roster testRoster = new Roster();
+		List<Roster> tr;
+		final String INTIAL_TEAM="1";
+		final String INTIAL_WEEK="1";
+		final String YEAR="2015";
+		final String League_ID="1682132";
+
+		tr=testRoster.getRoster(League_ID,INTIAL_TEAM,INTIAL_WEEK,YEAR);
+
 		//test complete player
 		Roster currPlayer= tr.get(0);
 		assertEquals(currPlayer.Players_PlayerID, 31743725);
@@ -126,26 +164,10 @@ public class TestApplicationTests {
 		assertEquals(currPlayer.NFLTeamName, "Miami Dolphins");
 		assertEquals(currPlayer.NFLTeam_NFLTeamID, 4);
 		System.out.println("Complete palyer test passes");
-		
-		
-		//test complete Roster
-		assertEquals(tr.get(0).Name, "Ryan Tannehill");
-		assertEquals(tr.get(1).Name, "Eric Decker");
-		assertEquals(tr.get(2).Name, "Jamaal Charles");
-		assertEquals(tr.get(3).Name, "Giovani Bernard");
-		assertEquals(tr.get(4).Name, "Dorial Green-Beckham");
-		assertEquals(tr.get(5).Name, "Martavis Bryant");
-		assertEquals(tr.get(6).Name, "Eric Ebron");
-		assertEquals(tr.get(7).Name, "Randall Cobb");
-		assertEquals(tr.get(8).Name,"Dan Bailey");
-		assertEquals(tr.get(9).Name, "Roddy White");
-		assertEquals(tr.get(10).Name, "Tom Brady");
-		assertEquals(tr.get(11).Name, "Alshon Jeffery");
-		System.out.println("Complete roster test passes");		
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 }
