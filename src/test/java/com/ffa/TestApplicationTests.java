314cf1c71b3316acfc0f0a69d50c0aa2e825adf1
Joseph Bauer <jwbauer3@wisc.edu>
2016-04-05 20:27:49 -0500

    added very basic test frame work test methods for FTJ and Roster

--- src/test/java/com/ffa/TestApplicationTests.java
+++ src/test/java/com/ffa/TestApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.ffa.controllers.Application;
+import com.ffa.models.FtjStats;
+import com.ffa.models.Roster;
 
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,4 +20,29 @@ public class TestApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testFTJ(){
+		int adrianPeterson = 1966936255;
+		int eddieLacy= 1221912022;
+		
+		FtjStats ftj = new FtjStats();
+		
+		boolean unfair = ftj.isFair(adrianPeterson, eddieLacy);
+		
+		assert(!unfair);
+		System.out.println("Unfair trade logic pass");
+		//assert(unfair);
+				
+	}
+	
+	@Test
+	public void testRosterModel(){
+		Roster testRoster = new Roster();
+		testRoster.getRoster("1", "1", "1", "2015");
+		System.out.println(testRoster.Name);
+		
+	}
+	
+	
+	
 }
