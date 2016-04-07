1d0bf9e5d05d1dd72817bb9d595e1398c70119af
Jake Trotman <trotman23@gmail.com>
2016-04-06 21:21:05 -0500

    jadaskljhasdfdfkdsfal

---
+++ src/main/java/com/ffa/models/CompositeRankings.java
@@ -0,0 +1,13 @@
+package com.ffa.models;
+
+public class CompositeRankings extends Rankings {
+
+	public int cRank;
+	public int cWins;
+	public int cLosses;
+	public int cTies;
+	
+	public CompositeRankings(){
+		
+	}
+}
