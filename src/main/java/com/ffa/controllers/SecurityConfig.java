8342800b47da8036f2307ca8186fd8de18e52cb2
Isaac Hoffmann <hoffmannisaac@gmail.com>
2016-05-02 15:54:26 -0500

    Removed the unused auth scheme

--- src/main/java/com/ffa/controllers/SecurityConfig.java
+++ src/main/java/com/ffa/controllers/SecurityConfig.java
@@ -30,8 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+        
     }
 }
