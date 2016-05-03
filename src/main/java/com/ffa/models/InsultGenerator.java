2058aa8b1df34e2b487ca886e4d28bee1150d3bf
Joseph Bauer <jwbauer3@wisc.edu>
2016-05-02 21:48:37 -0500

    a couple more

--- src/main/java/com/ffa/models/InsultGenerator.java
+++ src/main/java/com/ffa/models/InsultGenerator.java
@@ -8,8 +8,10 @@ final String[] INSULTS= {"This team drafts worst then the browns", "0-16??",
 		"This team looks worst then Johnny Manziel after a weekend in vegas", 
 		"This team lacks faith I would consider picking up Tim Tebow", 
 		"Maybe this team should stick to European Football", "This isn't golf", 
-		"This team is almost Trotty bad", "This team makes these insults look good"
-		};
+		"This team is almost Trotty bad", "This team makes these insults look good",
+		"This team makes Jay Cutler look good", "This team is like Peyton isn the playoffs",
+		"This team should use auto-draft next year", "This team performs as well as Apple Maps",
+		"I've seen better from Joel Stave", "This team should just move on to next year"};
 	String teamID;
   public InsultGenerator(String teamID) {
 		// TODO Auto-generated constructor stub
